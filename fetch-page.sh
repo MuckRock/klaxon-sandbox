b079b09662a1bc707ab1ff06500e049fe23d3f9a
Chris Amico <eyeseast@gmail.com>
2026-02-24 12:02:25 -0500

    include ourselves

--- fetch-page.sh
+++ fetch-page.sh
@@ -43,6 +43,7 @@ wget \
     --directory-prefix="$OUTDIR" \
     --adjust-extension \
     --tries=3 \
+    --wait=1 \
     --waitretry=2 \
     --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
     "$URL" || {
